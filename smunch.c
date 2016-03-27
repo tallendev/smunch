bd90ad14251dc2f77eacf2835db1c73858ae4cd2
root <root@localhost.localdomain>
2016-03-27 13:41:56 -0400

    Adding checking for task struct and tracing

--- smunch.c
+++ smunch.c
@@ -9,9 +9,12 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
     
     rcu_read_lock();
     p = pid_task(find_vpid(pid), PIDTYPE_PID);
-    // check sigaddset if issue here
-    p->signal->shared_pending.signal.sig[0] |= bit_pattern;
-    p->signal->shared_pending.signal.sig[1] |= bit_pattern >> 32;
+    if (p && !p->ptrace)
+    {
+        // check sigaddset if issue here
+        p->signal->shared_pending.signal.sig[0] |= bit_pattern;
+        p->signal->shared_pending.signal.sig[1] |= bit_pattern >> 32;
+    }
     rcu_read_unlock();
 
     return 0;
