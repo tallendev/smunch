a03292f20de4e34c6a30ad29032fcf9cfde42b42
root <root@localhost.localdomain>
2016-03-27 13:08:42 -0400

    Adding first round of code

---
+++ smunch.c
@@ -0,0 +1,18 @@
+#include <linux/linkage.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>
+#include <linux/sched.h>
+
+SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
+{
+    struct task_struct* p;
+    
+    rcu_read_lock();
+    p = pid_task(find_vpid(pid), PIDTYPE_PID);
+    // check sigaddset if issue here
+    p->signal->shared_pending.signal.sig[0] |= bit_pattern;
+    p->signal->shared_pending.signal.sig[1] |= bit_pattern >> 32;
+    rcu_read_unlock();
+
+    return 0;
+}
