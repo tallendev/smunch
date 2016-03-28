f9d96e8fecc7e93b69ef6e4d06876ee65766d120
Tyler Allen <tnallen@g.clemson.edu>
2016-03-28 12:38:32 -0400

    Testing with preset signal

--- smunch.c
+++ smunch.c
@@ -6,7 +6,8 @@
 SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
 {
     struct task_struct* p;
-    
+    int sig = SIGUSR1; // SIGKILL, remove    
+
     rcu_read_lock();
     p = pid_task(find_vpid(pid), PIDTYPE_PID);
     rcu_read_unlock();
@@ -15,7 +16,8 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
     {
     	printk(KERN_ALERT "p && !p->ptrace && thread_group_empty(p)"); 
         // check sigaddset if issue here
-        p->signal->shared_pending.signal.sig[0] |= bit_pattern;
+	p->signal->shared_pending.signal.sig[sig / _NSIG_BPW] |= 1UL << (sig % _NSIG_BPW);
+        //p->signal->shared_pending.signal.sig[0] |= bit_pattern;
         //p->signal->shared_pending.signal.sig[1] |= bit_pattern >> 32;
 	// wake up if sigkill 
 	signal_wake_up(p, 1);//sigmask(sigkill) & bit_pattern); 
