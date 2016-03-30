2f45a63f4060f787e241f3285da441fab0d8570b
root <root@localhost.localdomain>
2016-03-30 19:55:58 -0400

    Possibly final version

--- smunch.c
+++ smunch.c
@@ -5,52 +5,42 @@
 
 SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
 {
-    int wstatus;
+    unsigned long flags;
     struct task_struct* p;
     bool sigkill = bit_pattern & sigmask(SIGKILL);
-    if (sigmask(SIGKILL) & bit_pattern)
-    {
-        printk(KERN_ALERT "SIGKILL RECV");
-    }
     rcu_read_lock();
     p = pid_task(find_vpid(pid), PIDTYPE_PID);
     rcu_read_unlock();
-    printk(KERN_ALERT "pid_task yo");
-    if (p && !p->ptrace && thread_group_empty(p))
+    if (p && !p->ptrace && thread_group_empty(p) && 
+             lock_task_sighand(p, &flags))
     {
-    	printk(KERN_ALERT "p && !p->ptrace && thread_group_empty(p)"); 
         // check sigaddset if issue here
         if (p->exit_state == EXIT_ZOMBIE)
-	{
-	    printk(KERN_ALERT "proc is zombie");
+	    {
             if (sigkill)
-	    {
-		release_task(p);
-		printk(KERN_ALERT "task released");
-	    }
-	    else
-	    {
-	        return 0;
+	        {
+                unlock_task_sighand(p, &flags);
+                release_task(p);
+                lock_task_sighand(p, &flags);
+	        }
+	        else
+	        {
+                unlock_task_sighand(p, &flags);
+	            return 0;
             }
-	}
-	if (sigkill && p->state == TASK_UNINTERRUPTIBLE)
-	{
-		printk(KERN_ALERT "task sleepy");
-		p->state = TASK_INTERRUPTIBLE;
-		printk(KERN_ALERT "task interruptible");
-	}
-	printk(KERN_ALERT "loading bits");
-	p->signal->shared_pending.signal.sig[0] |= bit_pattern;
-	printk(KERN_ALERT "loaded bits");
-	signal_wake_up(p, sigkill); 
-	printk(KERN_ALERT "wake up yo");
+	    }
+        if (sigkill && p->state == TASK_UNINTERRUPTIBLE)
+        {
+            p->state = TASK_INTERRUPTIBLE;
+        }
+        p->signal->shared_pending.signal.sig[0] |= bit_pattern;
+        unlock_task_sighand(p, &flags);
+        signal_wake_up(p, sigkill); 
     }
     else
     {
-    	printk(KERN_ALERT "p && !p->ptrace && thread_group_empty(p)"); 
         return -1;
     }
-    printk(KERN_ALERT "done..."); 
 
     return 0;
 }
