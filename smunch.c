9e02646094dd2a85589cae5de8e41bca040706cd
Tyler Allen <tnallen@g.clemson.edu>
2016-03-30 20:12:57 -0400

    Checking if necessary to deliver signals after zombie check

--- smunch.c
+++ smunch.c
@@ -17,14 +17,19 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
         if (p->exit_state == EXIT_ZOMBIE)
 	    {
             unlock_task_sighand(p, &flags);
-            if (sigkill)
-	        {
-                release_task(p);
-                lock_task_sighand(p, &flags);
-	        }
-	        else
+            if (!sigkill)
 	        {
 	            return 0;
+	        }
+            release_task(p);
+            // TODO: test if lock can even be acquired:
+            if (lock_task_sighand(p, &flags))
+            {
+                printk(KERN_ALERT "GOT LOCK BACK");
+            }
+            else
+            {
+                printk(KERN_ALERT "didn't  get lock back might as well die");
             }
 	    }
         if (sigkill && p->state == TASK_UNINTERRUPTIBLE)
