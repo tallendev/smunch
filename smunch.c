2f5a8045469b42b2efeb787a61086fd0042b2b58
Tyler Allen <tnallen@g.clemson.edu>
2016-04-01 17:40:01 -0400

    final version?

--- smunch.c
+++ smunch.c
@@ -7,34 +7,34 @@ SYSCALL_DEFINE2(smunch, int, pid, unsigned long, bit_pattern)
 {
     unsigned long flags;
     struct task_struct* p;
-    bool sigkill = bit_pattern & sigmask(SIGKILL);
     rcu_read_lock();
     p = pid_task(find_vpid(pid), PIDTYPE_PID);
     rcu_read_unlock();
     if (p && !p->ptrace && thread_group_empty(p) && 
              lock_task_sighand(p, &flags))
     {
-        if (p->exit_state == EXIT_ZOMBIE)
-	    {
-            unlock_task_sighand(p, &flags);
-            if (sigkill)
-	        {
-                release_task(p);
-	        }
-	        return 0;
-	    }
-        if (sigkill && p->state == TASK_UNINTERRUPTIBLE)
+        if (bit_pattern & sigmask(SIGKILL))
         {
-            p->state = TASK_INTERRUPTIBLE;
+            if (p->exit_state == EXIT_ZOMBIE)
+            {
+                unlock_task_sighand(p, &flags);
+                release_task(p);
+                return 0;
+            }
+            p->signal->shared_pending.signal.sig[0] |= sigmask(SIGKILL);
+            unlock_task_sighand(p, &flags);
+            wake_up_process(p);
         }
-        p->signal->shared_pending.signal.sig[0] |= bit_pattern;
-        unlock_task_sighand(p, &flags);
-        signal_wake_up(p, sigkill); 
+        else
+        {
+            p->signal->shared_pending.signal.sig[0] |= bit_pattern;
+            unlock_task_sighand(p, &flags);
+            signal_wake_up(p, 0); 
+        }
     }
     else
     {
         return -1;
     }
-
     return 0;
 }
